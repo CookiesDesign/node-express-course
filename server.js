6d1d682dfd22265cebfe76e7f762d93f07f0747a
CookiesDesign <contactocookiesdesign@gmail.com>
2021-06-26 14:28:03 -0500

    Add first GET route

--- server.js
+++ server.js
@@ -3,9 +3,24 @@ const express = require('express');
 //La siguiente crea una instancia del constructor express, que llamaremos "app".
 const app = express();
 
-app.listen(8000,function(){
-    console.log("server is running")
+
+const mockUserData=[
+    {name:'Esmeralda'},
+    {name:'Madera'}
+    ]
+    
+    app.get('/users', function(req,res){
+         res.json({
+              success: true,
+              message: 'successfully got users. Nice!',
+              users: mockUserData
+         })
     })
+    
+
+
+
+app.listen(8000,function(){ console.log("server is running")})
 
 //Para ejecutar el archivo server.js se debe ingresar lo siguiente en la terminal: node server.js
 
