f80f26c10f8f746ed8fb065473831f5163c3e623
CookiesDesign <contactocookiesdesign@gmail.com>
2021-06-26 14:36:47 -0500

    Add login POST route

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 //La siguiente crea una instancia del constructor express, que llamaremos "app".
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 
 const mockUserData=[
     {name:'Esmeralda'},
@@ -26,6 +29,28 @@ const mockUserData=[
             user: req.params.id
         })
     })
+
+    app.post('/login',function(req,res){
+        const username=req.body.username;
+        const password=req.body.password;
+    
+        const mockUsername="billyTheKid";
+        const mockPassword="superSecret";
+    
+        if (username===mockUsername && password===mockPassword){
+             res.json({
+                  success: true,
+                  message: 'password and username match!',
+                  token: 'encrypted token goes here'
+             })
+        } else {
+             res.json({
+                  success: false,
+                  message: 'password and username do not match'
+             })
+        }
+   })
+   
     
 
 
